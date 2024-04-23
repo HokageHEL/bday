b59e2f2958d59dc67ac90631e92e327dcc81a1cd
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-04-23 16:10:55 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Confetti from "canvas-confetti";
+import "./App.css";
+import { msg } from "./msg";
+import oneCoin from "./img/Remove-bg.ai_1713877197022.png";
 
 function App() {
+  const [congratulations, setCongratulations] = useState(
+    "тут буде текст привітання"
+  );
+
+  const handleClick = async () => {
+    Confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+
+    const rnd = Math.floor(Math.random() * 86);
+
+    setCongratulations(msg[rnd]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="header">
+          <h1>З днем народження!</h1>
+        </header>
+        <main className="main">
+          <p className="main__text1">
+            Ця апка генерує привітання при кліці на кнопку нижче, спробуй!
+          </p>
+          <button
+            id="emitter"
+            className="main__button button-1"
+            onClick={() => handleClick()}
+          >
+            Привітання!
+          </button>
+
+          <div className="main__text">
+            <p className="main__text-text">{congratulations}</p>
+          </div>
+
+          <div>
+            <img src={oneCoin} alt="копійка" />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
